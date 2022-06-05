8efe3a0ff9852ed3cae5515b0dcb1f3b91425cd9
osund <osundal83@gmail.com>
2022-06-06 00:35:15 +0200

    add RowElementBySearchController and model

---
+++ commander64-backend/Controllers/RowElementBySearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using commander64_backend.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace commander64_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GetRowElementBySearchController : ControllerBase
+    {
+
+        private static List<RowElement> testData = new()
+        {
+            new RowElement(){Domain = "Typescript", Description = "Boilerplate123", Snippet = "export default = (): JSX.Element => { return ( ) }"},
+            new RowElement(){Domain = "C#", Description = "Boilerplatec#", Snippet = "Hartford"},
+            new RowElement(){Domain = "Powershell", Description = "Boilerplateps", Snippet = "Tampa"}
+        };
+        
+        [HttpGet]
+        public IEnumerable<RowElement> Get()
+        {
+            return testData;
+        }
+    }
+}
---
+++ commander64-backend/models/RowElement.cs
@@ -0,0 +1,9 @@
+﻿namespace commander64_backend.models
+{
+    public class RowElement
+    {
+        public string Domain { get; set; }
+        public string Description { get; set; }
+        public string Snippet { get; set; }
+    }
+}
